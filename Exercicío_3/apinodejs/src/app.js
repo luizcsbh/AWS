ea15b6067a67d46bad5513c099375cabfbef572f
Luiz Santos <luizcsbh@gmail.com>
2019-06-15 23:33:09 -0300

    Atualização

--- Exercicío_3/apinodejs/src/app.js
+++ Exercicío_3/apinodejs/src/app.js
@@ -1,18 +1,20 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var app = express();
+
 const router = express.Router();
+
 //Rotas
-const index = require('./routes/index');
-const personRouter = require('./routes/personRouter');
-const produtoRouter = require ('./routes/produtoRouter');
+const index = require('./routers/index');
+const personRouter = require('./routers/personRoute');
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', index);
 app.use('/person', personRouter);
-app.use('/produto', produtoRouter);
+
 
 module.exports = app;
---
+++ Exercicío_4/Cadastro Produto/store/Products.js
@@ -0,0 +1,17 @@
+const knex = require('knex')
+const knexConfigs = require('../knexfile')
+
+module.exports = {
+    get() {
+        
+    },
+    post(){
+        
+    },
+    delete(product) {
+        
+    },
+    update(product) {
+      
+    }
+}
